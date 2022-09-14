782c78e32e7836b78825e2a0c5591ba23d2f8fc4
Felix <bosirefelix97@gmail.com>
2022-09-14 13:48:49 +0300

    committed

---
+++ pretty web/app.py
@@ -0,0 +1,10 @@
+list=[1,2,5,8,7,9,2,1,5,54,56,4,6,6,6,6,6]
+
+new= list.reverse()
+try:
+    print(new)
+except:
+    print("Error")
+
+
+
--- pretty web/python.py
+++
@@ -1,5 +0,0 @@
-list=[1,2,5,8,7,9,2,1,5,54,56,4,6,6,6,6,6]
-new_list= list.sort()
-
-print(new_list)
-
